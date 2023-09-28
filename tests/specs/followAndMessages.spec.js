2873b8f1cb52d0453f21ad26903e22cf6c1e87c2
anisanursekararum <anisanursekararum@gmail.com>
2023-09-28 22:39:07 +0700

    update spec

--- tests/specs/followAndMessages.spec.js
+++ tests/specs/followAndMessages.spec.js
@@ -2,7 +2,14 @@ const HomePage = require('../pageObjects/home.page')
 const UserProfile = require('../pageObjects/userProfile.page')
 const RoomChat = require('../pageObjects/roomChat.page')
 
-describe('Direct Messages', () => {
+describe('Instagram', () => {
+  it('Follow user Instagram', async () => {
+    await HomePage.clickMenuSearch();
+    await HomePage.typingSearch('arum.jpeg');
+    await HomePage.clickUserFirst();
+    await UserProfile.verifyName('Sekar Arum');
+    await UserProfile.clickFollow();
+  })
   it('Send messages to Instagram user', async () => {
     await HomePage.clickMenuSearch();
     await HomePage.typingSearch('arum.jpeg');
